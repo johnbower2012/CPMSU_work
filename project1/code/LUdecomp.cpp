46d1108b5529e536481c80aa1e9309a824f451e9
John Bower <bowerjoh@msu.edu>
2016-02-12 14:58:01 -0500

    Time read out now includes log for viewing slope

--- project1/code/LUdecomp.cpp
+++ project1/code/LUdecomp.cpp
@@ -1,4 +1,14 @@
-/*Note that LU decomp is not possible for all matrices.*/
+/***************************************************************
+Note that LU decomp is not possible for all matrices.
+This file is designed to solve a second order differential
+equation using LU decomposition of matrices. Given -u"(x) = f(x),
+u(1)=u(0)=0, we approximate -u"(x) = -(v_i+1 + v_i-1 - 2*v_i)/h^2,
+and solve A*v=f, where A is a tridiagonal matrix, v is our 
+numerical solution, and f is our f(x) evaluated at each x_i. Each 
+has dimensionality n, where n is our number of grid points. We 
+define h, our step size, as 1/(n+1) so as not to include the end 
+points, which we fix by definition.
+****************************************************************/
 
 #include<iostream>
 #include<cmath>
@@ -20,47 +30,74 @@ void Matrix_Uvy_solve(int size, double**& u, double*& v, double*& y);
 void Matrix_LUdecomp_solve_complete(int size, double**& a, double*& v, double*& func);
 
 int main(int argc, char* argv[]){
-	int i, j, k, n, size, timerfile_yesno, error_yesno;
-	int sizetimefile = 0;
+	int i, j, k, n, size, timer_data_nofile, error_yesno;
+	int sizefile = 0, sizeerror=0;
 	double h, time;
 	double* v_lu, *y, *func;
 	double** a, **u, **l;
 	char* outfilename;
 	clock_t start, finish;
-//Desired matrix size--output file name--'1' or '0' for timefile or numerical data file, respectively
+//Desired matrix size--output file name--'1' or '0' for error file--'2', '1', '0' for timefile, numerical, or no data file, respectively
 	if(argc<5){
-		cout << "Bad usage. Enter also matrix_size outfilename error_yesno timerfile_yesno ('1' or '0' for last two) on same line." << endl;
+		cout << "Bad usage. Enter also matrix_size outfilename error_yesno ('1' or '0') timer_data_nofile on same ('2','1', or '0') line." << endl;
 		exit(1);
 	}
 	else{
 		size = atoi(argv[1]);
 		outfilename = argv[2];
 		error_yesno = atoi(argv[3]);
-		timerfile_yesno = atoi(argv[4]);
-		ofile.open(outfilename);
+		timer_data_nofile = atoi(argv[4]);
+		if(timer_data_nofile==2||timer_data_nofile==1){
+			ofile.open(outfilename);
+		}
 	}
 
-//Here we adjust our planned executions for the timefile forloop	
-	do{
-		sizetimefile++;
-		j = size/pow(10.0,sizetimefile);
-	} while(j>1);
-	if(j==1) sizetimefile*=2;
-	if(j==0) sizetimefile = 2*sizetimefile - 1;	
+/***************************************************************
+Here the process of filtering the input data occurs.
+If timer_data_nofile = 2, we created the appropriate limits for
+the time file, more information below. If timer_data_nofile = 1, 
+we set the forloop for one pass and merely set n to the desired 
+size. If timer_data_nofile equals 0, we forgo file creation 
+altogether for the loop. If error_yesno = 1, we set the 
+appropriate size for sizeerror for the error loop. If error_yesno
+equals 0, we forgo sizing sizeerror altogether since the loop 
+will not be executed.
+****************************************************************/
+	
+	if(timer_data_nofile==2){
+		do{
+			sizefile++;
+			j = size/pow(10.0,sizefile);
+		} while(j>1);
+		if(j==1) sizefile*=2;
+		if(j==0) sizefile = 2*sizefile - 1;
+	}
+	else if(timer_data_nofile==1){
+		n=size;
+		sizefile=1;
+	}
+	else if(timer_data_nofile==0){
+		sizefile=0;
+	}
+	if(error_yesno==1){
+		do{
+			sizeerror++;
+			j=size/pow(10.0,sizeerror);
+		} while (j>1);
+	}
+		
 /***************************************************************
 Note the structure presented here. If a time file is called for, 
 we perform the loops at n = 5, 10, 50, 100, 500, 100, etc until 
 the first number in this series greater than or equal to the 
 desired matrix size, writing each respective n and time to the
 entered outfilename. If a numerical data file is called for, we
-simply perform one loop and output the data itself.
+simply perform one loop and output the data itself. If no file
+is called, the loop does not execute.
 ****************************************************************/
-	for(k=1;k<=sizetimefile;k++){
-		if(timerfile_yesno==0){
-			k=sizetimefile;
-			n = size;
-		}
-		else if(timerfile_yesno==1){
+	
+	for(k=1;k<=sizefile;k++){
+		if(timer_data_nofile==2){
 			if(k%2==0) n=pow(10.0,k/2);
 			else n=pow(10.0,(k+1)/2)/2;
 		}
@@ -68,10 +105,12 @@ simply perform one loop and output the data itself.
 		h=(1.0/(double) (n+1));
 	
 //Allocate the arrays and matrices
+
 		Array_alloc(v_lu, n); Array_alloc(y, n); Array_alloc(func, n);
 		Matrix_alloc(a, n, n); Matrix_alloc(u, n, n); Matrix_alloc(l, n, n);
 
 //Initialize func[i] values && a[i][j]
+
 		for(i=0;i<n;i++){
 			func[i]=pow(h, 2.0)*100.0*exp(-10.0*h*((double) (i+1)));
 		}
@@ -83,48 +122,61 @@ simply perform one loop and output the data itself.
 			}
 		}
 
-
 /*********************
 Begin computation time
 **********************/
+
 		start = clock();
 
-//Here we conduct the decomp. Calculate the first (second) row (column) of u (l), second (third) row (column) of u (l), and so forth to the n-1th row, then nth column, then nth row.
+//Here we conduct the decomp
+
 		Matrix_LUdecomp(n, a, l, u);
+
 //Calculate y from L*y=func	
+
 		Matrix_Lyf_solve(n, l, y, func);
+
 //Calculcate v_lu from U*v_lu=y
+
 		Matrix_Uvy_solve(n, u, v_lu, y);
 
 		finish = clock();
+
 /*******************
 End computation time
 ********************/
 
 		time = ((finish-start)/(double) CLOCKS_PER_SEC);
 
-		if(timerfile_yesno==0)	cout << '\t' << time << " seconds" << endl;
+		if(timer_data_nofile==1) cout << '\t' << time << " seconds" << endl;
 
 //Write to file
-		if(timerfile_yesno==0){
+
+		if(timer_data_nofile==2){
+			ofile.precision(6);
+			if(k==1) ofile << "#_n" << setw(15) << "log10(n)" << setw(15) << "time" << setw(15) << "log10(time)" << endl;
+			ofile << n << setw(15) << log10(n) << setw(15) << time << setw(15) << log10(time) << endl;
+		}		
+		else if(timer_data_nofile==1){
 			for(i=0;i<n;i++){
 				ofile << h*(double) (i+1) << setw(20) << v_lu[i] << endl;
 			}
 		}
-		else if(timerfile_yesno==1){
-			ofile.precision(6);
-			ofile << n << setw(15) << time << endl;
-		}
-
+		
 		Array_dealloc(y); Array_dealloc(v_lu); Array_dealloc(func);
 		Matrix_dealloc(a,n); Matrix_dealloc(u,n); Matrix_dealloc(l,n);
 	}
 	
-	ofile.close();
+	if(timer_data_nofile==2||timer_data_nofile==1){
+		ofile.close();
+	}
 
-/**************************
-Calculate error if desired
-***************************/
+/***************************************************************
+Calculate error if desired, up to file size provided by input.
+Note that it outputs maximum error for 2,3,4,...,9,10,20,30...,
+90,100,200,... up to the 10^(ceiling(power of ten of input)),
+minimum of 10^1.
+****************************************************************/
 
 
 	if(error_yesno==1){
@@ -133,7 +185,7 @@ Calculate error if desired
 		ofile << "#_n" << setw(15) << "log(h)" << setw(20) << "max_log(error)" << endl;
 		double exact, x, temp, error;
 		int m;
-		for(m=0;m<3;m++){
+		for(m=0;m<sizeerror;m++){
 			for(k=2;k<=10;k++){
 				n=k*pow(10,m);
 				h=(1.0/(double) (n+1));
--- project1/code/tridiag.cpp
+++ project1/code/tridiag.cpp
@@ -1,17 +1,28 @@
-/* 
-Here we have a second order differential equation, solved numerically by use of grid points. 
-We have -u"(x) = f(x), x from (0,1), u(0)=u(1)=0, f(x) = 100exp(-10x). 
-We solve this by transforming it into a linear algebra problem, wherein -(v_i+1 + v_i-1 - 2*v_i)/(h^2) = f_i. 
-We first solve it as a general tridiagonal matrix with arrays a, b, c, then for our specific case where a_ii=2,
-a_(|i-j|=1)=1, and a_(|i-j|>1)=0.
+/***************************************************************
+This file is designed to solve a second order differential
+equation by discretizing the domain into x_i's and forming a 
+linear algebra problem. Given -u"(x) = f(x), u(1)=u(0)=0, we 
+approximate -u"(x_i) = -(v_i+1 + v_i-1 - 2*v_i)/h^2 = f(x_i), and 
+solve A*v=f, where A is a tridiagonal matrix, v is an array of 
+our numerical solution, and f is an array of our f(x_i). Each has 
+dimensionality n, where n is our number of grid points. We 
+define h, our step size, as 1/(n+1) so as not to include the end 
+points, which we fix by definition.
+
+Here we simplify the problem due to A's tridiagonal form, first
+solving for a general tridiagonal matrix, then recognizing the 
+inherent symmetry to reduce the number of floating points 
+operatings required by half. We speed the process by allocating 
+space for only three arrays to represent the diagonal and 
+off-diagonal elements, as shown below. 
 
 A = ( b_1 c_1  0   0   0  ...  0   0  )
     ( a_2 b_2 c_2  0   0  ...  0   0  )
     (  0  a_3 b_3 c_3  0  ...  0   0  )
     ( ... ... ... ... ... ... ... ... )
     (  0   0   0   0  ...  0  a_n b_n )
-*/
 
+****************************************************************/
 
 #include<iostream>
 #include<cmath>
@@ -23,46 +34,69 @@ using namespace std;
 
 ofstream ofile;
 
-
-/*
-Functions to condense the main() function. 
-The first is "general tridiagonal matrix equation solver."
-The second is "second derivative ----," made for our specific form of the second derivative.
-*/
 void Array_alloc(double*& a, int length);
 void Matrix_alloc(double**& a, int rows, int columns);
 void Array_dealloc(double*& a);
 void Matrix_dealloc(double**& a, int rows);
-void gentridmat_equ_solve(int n, double*& a, double*& b, double*& c, double*& v, double*& func);
+void gen_tridmat_equ_solve(int n, double*& a, double*& b, double*& c, double*& v, double*& func);
 void secder_tridmat_equ_solve(int n, double*& b, double*& v, double*& func);
 
 int main(int argc, char* argv[]){
 	char* outfilename;
-	int n, i, j, k, error_yesno, timerfile_yesno, size;
-	int sizetimefile = 0;
+	int n, i, j, k, error_yesno, timer_data_nofile, size;
+	int sizefile = 0, sizeerror=0;
 	double h, time1, time2;
 	double* a, *b, *c, *v1, *v2, *func;
 	clock_t start, finish;
 
 	if(argc<5){
-		cout << "Bad usage. Include also in command line gridpoint_count outfilename '1 or 0 (for error)' '1 or 0' for time file" << endl;
+		cout << "Bad usage. Enter also grid_point_count outfilename error_yesno ('1' or '0') timer_data_nofile on same ('2','1', or '0') line." << endl;
 		exit (1);
 	}
 	else{
 		size = atoi(argv[1]);
 		outfilename = argv[2];
 		error_yesno = atoi(argv[3]);
-		timerfile_yesno = atoi(argv[4]);
-		ofile.open(outfilename);
+		timer_data_nofile = atoi(argv[4]);
+		if(timer_data_nofile==2||timer_data_nofile==1){
+			ofile.open(outfilename);
+		}
 	}
 
-//Here we adjust our planned executions for the timefile forloop	
-	do{
-		sizetimefile++;
-		j = size/pow(10.0,sizetimefile);
-	} while(j>1);
-	if(j==1) sizetimefile*=2;
-	if(j==0) sizetimefile = 2*sizetimefile - 1;
+/***************************************************************
+Here the process of filtering the input data occurs.
+If timer_data_nofile = 2, we created the appropriate limits for
+the time file, more information below. If timer_data_nofile = 1, 
+we set the forloop for one pass and merely set n to the desired 
+size. If timer_data_nofile equals 0, we forgo file creation 
+altogether for the loop. If error_yesno = 1, we set the 
+appropriate size for sizeerror for the error loop. If error_yesno
+equals 0, we forgo sizing sizeerror altogether since the loop 
+will not be executed.
+****************************************************************/
+	
+	if(timer_data_nofile==2){
+		do{
+			sizefile++;
+			j = size/pow(10.0,sizefile);
+		} while(j>1);
+		if(j==1) sizefile*=2;
+		if(j==0) sizefile = 2*sizefile - 1;
+	}
+	else if(timer_data_nofile==1){
+		n=size;
+		sizefile=1;
+	}
+	else if(timer_data_nofile==0){
+		sizefile=0;
+	}
+	if(error_yesno==1){
+		do{
+			sizeerror++;
+			j=size/pow(10.0,sizeerror);
+		} while (j>1);
+	}
+
 /***************************************************************
 Note the structure presented here. If a time file is called for, 
 we perform the loops at n = 5, 10, 50, 100, 500, 100, etc until 
@@ -71,27 +105,21 @@ desired matrix size, writing each respective n and time to the
 entered outfilename. If a numerical data file is called for, we
 simply perform one loop and output the data itself.
 ****************************************************************/
-	for(k=1;k<=sizetimefile;k++){
-		if(timerfile_yesno==0){
-			k=sizetimefile;
-			n = size;
-		}
-		else if(timerfile_yesno==1){
+
+	for(k=1;k<=sizefile;k++){
+		if(timer_data_nofile==2){
 			if(k%2==0) n=pow(10.0,k/2);
 			else n=pow(10.0,(k+1)/2)/2;
 		}
 	
 		h=(1.0/(double) (n+1));
 
-//Dynamically allocate memory, with check.	
-		Array_alloc(a, n); Array_alloc(b,n); Array_alloc(c,n); Array_alloc(v1,n); Array_alloc(v2,n); Array_alloc(func, n);
+//Dynamically allocate memory
+	
+		Array_alloc(a,n); Array_alloc(b,n); Array_alloc(c,n); Array_alloc(v1,n); Array_alloc(v2,n); Array_alloc(func, n);
 
-		if(a==nullptr||b==nullptr||c==nullptr||v1==nullptr||v2==nullptr||func==nullptr){
-			cout << "Error allocating dynamic memory. Terminating...\n";
-			exit(1);
-		}
+//Initialize array values
 
-//Initialize array values.
 		for(i=0;i<n;i++){
 			a[i]=(-1.0);
 			b[i]=(2.0);
@@ -99,31 +127,34 @@ simply perform one loop and output the data itself.
 			func[i]=pow(h, 2.0)*100.0*exp(-10.0*h*((double) (i+1)));
 		}
 
-/*
+/********************************************
   Start clock for general tridiagonal matrix
   Create upper diagonal matrix
   Back substitutions
   End clock
-*/
+*********************************************/
+
 		start = clock();
 	
-		gentridmat_equ_solve(n, a, b, c, v1, func);
+		gen_tridmat_equ_solve(n, a, b, c, v1, func);
 
 		finish = clock();
 		time1 = ( (finish - start)/(double) CLOCKS_PER_SEC);
 
-//redfine b values for our specific tridiagonal, reinitialize func[i] values.
+//redfine b values for our specific tridiagonal, reinitialize func[i] values
+
 		for(i=0;i<n;i++){
 			b[i]=(double) (i+2)/(double) (i+1);
 			func[i]=pow(h, 2.0)*100.0*exp(-10.0*h*((double) (i+1)));
 		}
 
-/*
+/********************************************
   Start clock for our specific tridiagonal
   Form A into upper diagonal
   Back substitutions
   End clock
-*/
+*********************************************/
+
 		start = clock();
 
 		secder_tridmat_equ_solve(n, b, v2, func);
@@ -132,7 +163,16 @@ simply perform one loop and output the data itself.
 		time2 = ((finish-start)/(double) CLOCKS_PER_SEC);
 
 //Write data file
-		if(timerfile_yesno==0){
+
+		if(timer_data_nofile==2){
+			ofile.precision(6);
+			if(k==1){
+				ofile << "#_n" << setw(15) << "log10(n)" << setw(15) << "time_gen" << setw(15);
+				ofile << "log10(time_gen)" << setw(15) << "time_our" << setw(15) << "log10(time_our)" << endl;	
+			}						
+			ofile << n << setw(15) << log10(n) << setw(15) << time1 << setw(15)<< log10(time1) << setw(15) << time2 << setw(15) << log10(time2) << endl;
+		}
+		else if(timer_data_nofile==1){
 			ofile << "#_x_i" << setw(15) << "v1(x_i)" << setw(15) << "v2(x_i)" << setw(15) << "u(x_i)" << endl;
 			ofile.precision(8);		
 			ofile << "1" << setw(15) << "0" << setw(15) << "0" << endl;
@@ -144,17 +184,18 @@ simply perform one loop and output the data itself.
 			}
 			ofile << "0" << setw(15) << "0" << setw(15) << "0" << endl;
 		}
-		else if(timerfile_yesno==1){
-			ofile.precision(6);			
-			ofile << n << setw(15) << time1 << setw(15) << time2 << endl;
-		}
 		
-	Array_dealloc(a); Array_dealloc(b); Array_dealloc(c); Array_dealloc(v1); Array_dealloc(v2); Array_dealloc(func);
+		Array_dealloc(a); Array_dealloc(b); Array_dealloc(c);
+		Array_dealloc(v1); Array_dealloc(v2); Array_dealloc(func);
+	}
+	
+	if(timer_data_nofile==2||timer_data_nofile==1){
+		ofile.close();
 	}
 
-	ofile.close();
+//Write time to screen if creating a data file
 	
-	if(timerfile_yesno==0){
+	if(timer_data_nofile==1){
 		cout.precision(5);
 		cout << scientific;
 		cout << "\tv1: " << time1 << " seconds" << endl;
@@ -162,22 +203,20 @@ simply perform one loop and output the data itself.
 	}
 
 //Compute error for varying step counts, if desired
+
 	if(error_yesno==1){
 		ofile.open("trid_error.dat");
 		ofile.precision(8);
 		ofile << "#_n" << setw(15) << "log(h)" << setw(20) << "max_log(error1)" << setw(15) << "max_log(error2)" << endl;
 		double exact, x, temp1, temp2, error1, error2;
-		int k;
-		for(j=0;j<7;j++){
+		int m;
+		for(m=0;m<sizeerror;m++){
 			for(k=2;k<=10;k++){
-				n = k*pow(10,j);
+				n = k*pow(10,m);
 				double* a, *b1, *b2, *c, *func1, *func2, *v1, *v2;
 				Array_alloc(a,n); Array_alloc(b1,n); Array_alloc(b2,n); Array_alloc(c,n);
 				Array_alloc(func1,n); Array_alloc(func2,n); Array_alloc(v1,n); Array_alloc(v2,n);
-				if(a==nullptr||b1==nullptr||b2==nullptr||c==nullptr||func1==nullptr||func2==nullptr||v1==nullptr||v2==nullptr){
-					cout << "Unable to allocate error computation memory for " << n << " grid points. Terminating..." << endl;
-					exit(1);
-				}
+				
 				h = 1.0/((double) (n+1));
 				for(i=0;i<n;i++){
 					a[i]=(-1.0);
@@ -188,29 +227,29 @@ simply perform one loop and output the data itself.
 					func2[i]=pow(h, 2.0)*100.0*exp(-10.0*h*((double) (i+1)));
 				}	
 
-				gentridmat_equ_solve(n, a, b1, c, v1, func1);
+				gen_tridmat_equ_solve(n, a, b1, c, v1, func1);
 				secder_tridmat_equ_solve(n, b2, v2, func2);
 				
-		
 				error1=-20.0;
 				error2=-20.0;
 				for(i=0;i<n;i++){
 					temp1 = 0.0;
 					temp2 = 0.0;
-					x = (i+1)*h;
+					x = (double) (i+1)*h;
 					exact = 1.0 - (1.0 - exp(-10.0))*x-exp(-10.0*x);
 					temp1 = log10(abs(v1[i]-exact)/exact);
 					temp2 = log10(abs(v2[i]-exact)/exact);
 					if(temp1>error1) error1 = temp1;
 					if(temp2>error2) error2 = temp2;
 				}
-				ofile << n << setw(20-j) << log10(h) << setw(15) << error1 << setw(15) << error2 << endl;
+				ofile << n << setw(20-m) << log10(h) << setw(15) << error1 << setw(15) << error2 << endl;
 
 				Array_dealloc(a); Array_dealloc(b1); Array_dealloc(b2); Array_dealloc(c);
 				Array_dealloc(func1); Array_dealloc(func2); Array_dealloc(v1); Array_dealloc(v2);
 			}
 		}
 	}
+
 
 	return 0;
 }	
@@ -237,7 +276,7 @@ void Matrix_dealloc(double**& a, int rows){
 	delete[] a;
 }
 
-void gentridmat_equ_solve(int n, double*& a, double*& b, double*& c, double*& v, double*& func){
+void gen_tridmat_equ_solve(int n, double*& a, double*& b, double*& c, double*& v, double*& func){
 	int i;
 	for(i=1;i<n;i++){
 		double temp;
